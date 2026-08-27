679b17a95f39871ddcc4a91acd4248031c5b2935
Kavyarai-11a <kavyarai812@gmail.com>
2026-08-27 23:39:46 +0530

    Int_Float

---
+++ DSA_Class_Tasks/Int_Float.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+
+void safePrint(void *ptr, int type)
+{
+    if (ptr == NULL)
+    {
+        printf("NULL pointer\n");
+        return;
+    }
+
+    if (type == 0)
+    {
+        printf("Address = %p, Value = %d\n", ptr, *(int *)ptr);
+    }
+    else if (type == 1)
+    {
+        printf("Address = %p, Value = %.2f\n", ptr, *(float *)ptr);
+    }
+}
+
+int main()
+{
+    int arrI[10];
+    int arrF[10];
+    int type;
+
+    printf("Enter 10 integers:\n");
+
+        for (int i = 0; i < 10; i++)
+        {
+            scanf("%d", arrI + i);
+        }
+
+    printf("Enter 10 floating-point values:\n");
+
+        for (int i = 0; i < 10; i++)
+        {
+            scanf("%f", arrF + i);
+        }
+
+    printf("Enter type:\n");
+    printf("0 - Integer\n");
+    printf("1 - Float\n");
+    printf("Enter your choice: ");
+    scanf("%d", &type);
+
+    if (type == 0)
+    {
+        printf("\nInteger values and addresses:\n");
+
+        for (int i = 0; i < 10; i++)
+        {
+            safePrint(arrI + i, type);
+        }
+    }
+    else if (type == 1)
+    {
+        printf("\nFloat values and addresses:\n");
+
+        for (int i = 0; i < 10; i++)
+        {
+            safePrint(arrF + i, type);
+        }
+    }
+    else
+    {
+        printf("Invalid type!\n");
+    }
+
+    printf("\nTesting NULL pointer:\n");
+    safePrint(NULL, type);
+
+    return 0;
+}
