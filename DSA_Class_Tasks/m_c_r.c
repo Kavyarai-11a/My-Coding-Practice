2e3c5db024244d1e96304125503f0de5271e2467
Kavyarai-11a <kavyarai812@gmail.com>
2026-08-28 01:05:06 +0530

    heap_memory_concept

---
+++ DSA_Class_Tasks/m_c_r.c
@@ -0,0 +1,68 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main() {
+    int n;
+    int *p;
+    printf("Enter the size of array : ");
+    scanf("%d",&n);
+
+    int * arr_1 = (int *)malloc(n*sizeof(int));
+    p = arr_1;
+    printf("Enter %d element\n",n);
+    for(int i=0;i<n;i++) {
+        scanf("%d",arr_1 + i);
+    }
+
+    printf("\nArray elements and address\n");
+    for(int i=0;i<n;i++) {
+        printf("%d\n",*(arr_1 + i));
+        printf("%p\n",(arr_1 + i));
+    }
+
+    int * arr_2 = (int *)calloc(n,sizeof(int));
+
+    printf("\nInitial Array\n");
+    for(int i=0;i<n;i++) {
+        printf("%d\n",*(arr_2 + i));
+        printf("%p\n",(arr_2 + i));
+    }
+
+    for(int i=0;i<n;i++) {
+        *(arr_2 + i) = *(arr_1 + i);
+    }
+
+    printf("\nArray elements and address\n");
+    for(int i=0;i<n;i++) {
+        printf("%d\n",*(arr_2 + i));
+        printf("%p\n",(arr_2 + i));
+    }
+
+    int m;
+    printf("\nEnter the size of array greater than %d: ",n);
+    scanf("%d",&m);
+    arr_1 =(int *)realloc(arr_1,m*sizeof(int));
+
+    printf("\nEnter %d element\n",m-n);
+    for(int i=n;i<m;i++) {
+        scanf("%d",arr_1 + i);
+    }
+
+    printf("\nArray elements and address\n");
+    for(int i=0;i<m;i++) {
+        printf("%d\n",*(arr_1 + i));
+        printf("%p\n",(arr_1 + i));
+    }
+
+    if(p == arr_1) {
+        printf("Pointer does not changed");
+    }
+    else {
+        printf("Pointer changed");
+    }
+
+    free(arr_1);
+    free(arr_2);
+    free(p);
+
+    return 0;
+}
