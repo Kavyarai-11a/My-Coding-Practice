6baf6c32605f0b855e91c66d6c5a8ea1813b712b
Kavyarai-11a <kavyarai812@gmail.com>
2026-08-30 23:42:27 +0530

    Search

--- DSA_Class_Tasks/Search.c
+++ DSA_Class_Tasks/Search.c
@@ -12,4 +12,20 @@ int main() {
         return 1;
     }
 
+    int *arr = malloc(n * sizeof(int));
+    if(arr == NULL) {
+        printf("Memory not allocate\n");
+        return 1;
+    }
+
+    printf("Enter %d elements\n",n);
+    for(int i=0;i<n;i++) {
+        scanf("%d",(arr + i));
+        if(*(arr + i) < -1000000000 ||*(arr + i) > 1000000000) {
+            printf("Invalid Input\n");
+            free(arr);
+            return 1;
+        }
+    }
+
     
