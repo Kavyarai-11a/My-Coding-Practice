dfc7058f16f5442b0ea9804f9d007b6132221c1a
Kavyarai-11a <kavyarai812@gmail.com>
2026-08-30 23:43:46 +0530

    Search

--- DSA_Class_Tasks/Search.c
+++ DSA_Class_Tasks/Search.c
@@ -50,3 +50,13 @@ int main() {
     return 0;
 }
 
+int sequentialSearch (int* arr, int n, int target) {
+
+    for(int i=0;i<n;i++) {
+        if(*(arr + i) == target) {
+            return i;
+        }
+    }
+
+    return -1;
+}
