574e008338491764dd3e475ea8922c56dde49944
Kavyarai-11a <kavyarai812@gmail.com>
2026-08-31 23:40:13 +0530

    Structure

--- DSA_Class_Tasks/structure.c
+++ DSA_Class_Tasks/structure.c
@@ -12,4 +12,16 @@ int main() {
     printf("Enter the size of array: ");
     scanf("%d",&n);
 
+    Book * B;
+    B = malloc(n * sizeof(Book));
+
+    for(int i=0;i<n;i++) {
+        printf("Enter id of book %d : ",i + 1);
+        scanf("%d",&(B + i)->id);
+        printf("Enter title of book %d : ",i + 1);
+        scanf("%s",(B + i)->title);
+        printf("Enter price of %d book : ",i + 1);
+        scanf("%f",&(B + i)->price);
+    }
+
     
