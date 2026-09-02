90b9f95b45f5f3b0b6c942d8f7603e553f9e3cc8
Kavyarai-11a <kavyarai812@gmail.com>
2026-09-02 22:17:17 +0530

    Linked List taught in class

--- DSA_Class_Tasks/LinkedList.c
+++ DSA_Class_Tasks/LinkedList.c
@@ -37,3 +37,24 @@ int insertFront(Node **head,int data) {
 
     return 0;
 }
+
+//pushing node from wnd in linked list from scrach
+int insertEnd(Node **head,int data) {
+    Node * newNode = Createnode(data);
+    if(newNode == NULL) {
+        return -1;
+    }
+
+    if(*head == NULL) {
+        *head = newNode;
+    }
+
+    Node * temp;
+    temp = *head;
+    while(temp->link != NULL) {
+        temp = temp->link;
+    }
+    temp->link = newNode;
+
+    return 0;
+}
