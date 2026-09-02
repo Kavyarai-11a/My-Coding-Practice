5549b3d15fba6a917497b2ae68ed7942da5639fb
Kavyarai-11a <kavyarai812@gmail.com>
2026-09-02 19:27:18 +0530

    Linked List taught in class

--- DSA_Class_Tasks/LinkedList.c
+++ DSA_Class_Tasks/LinkedList.c
@@ -4,3 +4,17 @@ typedef struct node {
     int data;
     int *link;
 }Node;
+
+//Creating a new node
+Node * Createnode(int data) {
+
+    Node * newNode = (Node *)malloc(sizeof(Node));
+    if(newNode == NULL) {
+        return NULL;
+    }
+
+    newNode->data = data;
+    newNode->link = NULL;
+
+    return newNode;
+}
