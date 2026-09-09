69a0de651d897655e19befe8a5053be5caa25294
Kavyarai-11a <kavyarai812@gmail.com>
2026-09-10 01:37:06 +0530

    New commit

---
+++ JAVA/ArrSumAvg.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class ArrSumAvg {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n;
+		System.out.print("Enter size of array : ");
+		n = sc.nextInt();
+
+		int[] arr = new int[n];
+
+		int sum = 0;
+		System.out.println("Enter " + n + " elements");
+		for(int i=0;i<n;i++) {
+			arr[i] = sc.nextInt();
+			sum += arr[i];
+		}
+		
+		double avg = sum/n;
+		
+		System.out.println("Sum = " + sum);
+		System.out.println("Average = " + avg);
+
+		sc.close();
+	}
+}
