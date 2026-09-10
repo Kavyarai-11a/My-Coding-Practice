c16ae11c832a9d63791f1d9105a1a3d7dbc6a788
Kavyarai-11a <kavyarai812@gmail.com>
2026-09-10 21:32:35 +0530

    New commit

--- SQL_Task/foregin_key.sql
+++ SQL_Task/foregin_key.sql
@@ -1,5 +1,6 @@
 CREATE DATABASE Company;
 USE Company;
+Drop database Company;
 CREATE TABLE DEPARTMENT (
 DeptID VARCHAR(50) PRIMARY KEY,
 DeptName VARCHAR(50) DEFAULT "Employee"
@@ -9,8 +10,7 @@ EmpID VARCHAR(50) PRIMARY KEY,
 EmpName VARCHAR(50) NOT NULL,
 Email VARCHAR(50) UNIQUE,
 Salary INT CHECK(Salary > 10000),
-DeptID VARCHAR(50),
-FOREIGN KEY (DeptID) REFERENCES DEPARTMENT (DeptID)
+City varchar(50)
 );
 SHOW TABLES;
 DESCRIBE DEPARTMENT;
@@ -49,15 +49,33 @@ values
 (1,"a"),
 (2,"b");
 
-insert into EMPLOYEE (EmpID,EmpName,Email,Salary,DeptID)
+insert into EMPLOYEE (EmpID,EmpName,Email,Salary,City)
 values
-(1,"A","A@",100000,1),
-(2,"B","B@",100000,2);
+(1,"A","A@",100000,"Delhi"),
+(2,"B","B@",200000,"Pune");
 
 insert into Project(ProjectID,ProjectName,Budget,DeptID,status)
 values
 (1,"A",100000,1,"progressing"),
 (2,"B",100000,2,"stop");
+
+select * from EMPLOYEE where Salary > 100000;
+select * from EMPLOYEE where Salary > 100000 and city = "Pune";
+select * from EMPLOYEE where Salary > 100000 or city = "Pune";
+select * from EMPLOYEE where Salary between 100000 and 300000;
+select * from EMPLOYEE where City in ('Delhi','Pune');
+select * from EMPLOYEE where City not in ('Delhi');
+select * from EMPLOYEE where Salary > 150000;
+select * from EMPLOYEE where Salary < 150000;
+select * from EMPLOYEE where Salary != 100000;
+select * from EMPLOYEE where Salary <> 100000;
+select * from EMPLOYEE where Salary >= 100000;
+select * from EMPLOYEE where Salary <= 200000;
+select * from EMPLOYEE where Salary = 100000;
+select * from EMPLOYEE order by Salary asc;
+select * from EMPLOYEE order by Salary desc;
+select * from EMPLOYEE where City like "de";
+
 
 
 
