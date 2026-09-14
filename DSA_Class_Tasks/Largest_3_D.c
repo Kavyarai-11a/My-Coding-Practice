35c1a23931c555d413cb9890b1b24ca784690e54
Kavyarai-11a <kavyarai812@gmail.com>
2026-09-14 23:18:17 +0530

    New Commit

---
+++ DSA_Class_Tasks/Largest_3_D.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int lays,rows,cols;
+    printf("Enter sizes of array\n");
+    printf("Enter layers of array : ");
+    scanf("%d",&lays);
+    printf("Enter rows of array : ");
+    scanf("%d",&rows);
+    printf("Enter columns of array : ");
+    scanf("%d",&cols);
+
+    
