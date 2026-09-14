281915014cf9e782be45c8a3a2dff099358584a9
Kavyarai-11a <kavyarai812@gmail.com>
2026-09-14 23:20:20 +0530

    New Commit

--- DSA_Class_Tasks/Largest_3_D.c
+++ DSA_Class_Tasks/Largest_3_D.c
@@ -10,5 +10,19 @@ int main()
     scanf("%d",&rows);
     printf("Enter columns of array : ");
     scanf("%d",&cols);
+    int ***arr = malloc(lays * sizeof(int **));
+
+    for(int i=0;i<lays;i++)
+    {
+        arr[i] = malloc(rows * sizeof(int *));
+    }
+
+    for(int i=0;i<lays;i++)
+    {
+        for(int j=0;j<rows;j++)
+        {
+            arr[i][j] = malloc(cols * sizeof(int));
+        }
+    }
 
     
