4620ab9c2c4472f797d5fc5375ca1bb7228e12f0
Kavyarai-11a <kavyarai812@gmail.com>
2026-09-18 01:15:37 +0530

    New Commit

---
+++ JAVA/ConstructorE.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Employee {
+
+    double Salary;
+
+    Employee(double Salary) {
+        this.Salary = Salary;
+    }
+
+    void display() {
+        System.out.println("Salary = " + Salary);
+    }
+}
