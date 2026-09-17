dede94558faa6ad75489ac770f43d6c8aa8ba23e
Kavyarai-11a <kavyarai812@gmail.com>
2026-09-17 23:58:27 +0530

    NEW Commit

--- JAVA/WrapperDemo.java
+++ JAVA/WrapperDemo.java
@@ -12,6 +12,13 @@ class WrapperDemo {
         System.out.print("Enter marks of student 3 :");
         String s3 = sc.nextLine();
 
+        Integer i1 = Integer.valueOf(s1);
+        Integer i2 = Integer.valueOf(s2);
+        Integer i3 = Integer.valueOf(s3);
+
+        int total = i1 + i2 + i3;
+        double avg = total/(double)3;
+
         
     }
 }
