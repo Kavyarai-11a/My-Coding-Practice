0be08ffca820e42311203f22231cb5a1f49b5cf1
Kavyarai-11a <kavyarai812@gmail.com>
2026-09-17 23:56:00 +0530

    NEW Commit

---
+++ JAVA/WrapperDemo.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class WrapperDemo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter marks of student 1 : ");
+        String s1 = sc.nextLine();
+
+        System.out.print("Enter marks of student 2 :");
+        String s2 = sc.nextLine();
+
+        System.out.print("Enter marks of student 3 :");
+        String s3 = sc.nextLine();
+
+        
+    }
+}
