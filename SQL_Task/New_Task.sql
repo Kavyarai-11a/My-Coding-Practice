408166bec8f414c4741a2cf157f82aac20dc19bc
Kavyarai-11a <kavyarai812@gmail.com>
2026-09-21 23:02:36 +0530

    New Commit

--- SQL_Task/New_Task.sql
+++ SQL_Task/New_Task.sql
@@ -1,4 +1,5 @@
 create database Company;
+
 drop database Company;
 use Company;
 create table DEPARTMENT (
@@ -7,6 +8,7 @@ DNUMBER INT PRIMARY KEY,
 MGRSSN VARCHAR(50) unique,
 MGRSTARTDATE date
 );
+DROP TABLE EMPLOYEE;
 create table EMPLOYEE (
 FNAME VARCHAR(50) NOT NULL,
 MNAME VARCHAR(50) NOT NULL,
@@ -22,11 +24,39 @@ DNO INT,
 foreign key(DNO) references DEPARTMENT(DNUMBER)
 ); 
 
+INSERT INTO DEPARTMENT
+(DNAME, DNUMBER, MGRSSN, MGRSTARTDATE)
+VALUES
+('Research', 5, '333445555', '1988-05-22'),
+('Administration', 4, '987654321', '1995-01-01'),
+('Headquarters', 1, '888665555', '1981-06-19');
+
+INSERT INTO EMPLOYEE
+(FNAME, MNAME, LNAME, SSN, BDATE, ADDRESS, SEX, SALARY, SUPERSSN, DNO)
+VALUES
+('James', 'E', 'Borg', '888665555', '1937-11-10', '450 Stone, Houston, TX', 'M', 55000, NULL, 1),
+('Franklin', 'T', 'Wong', '333445555', '1955-12-08', '638 Voss, Houston, TX', 'M', 40000, '888665555', 5),
+('John', 'B', 'Smith', '123456789', '1965-01-09', '731 Fondren, Houston, TX', 'M', 30000, '333445555', 5),
+('Jennifer', 'S', 'Wallace', '987654321', '1941-06-20', '291 Berry, Bellaire, TX', 'F', 43000, '888665555', 4),
+('Alicia', 'J', 'Zelaya', '999887777', '1968-07-19', '3321 Castle, Spring, TX', 'F', 25000, '987654321', 4),
+('Ramesh', 'K', 'Narayan', '666884444', '1962-09-15', '975 Fire Oak, Humble, TX', 'M', 38000, '333445555', 5),
+('Joyce', 'A', 'English', '453453453', '1972-07-31', '5631 Rice, Houston, TX', 'F', 25000, '333445555', 5),
+('Ahmad', 'V', 'Jabbar', '987987987', '1969-03-29', '980 Dallas, Houston, TX', 'M', 25000, '987654321', 4);
+
 CREATE TABLE DEPT_LOCATIONS (
-DNUMBER INT PRIMARY KEY,
+DNUMBER INT,
 foreign key(DNUMBER) references DEPARTMENT(DNUMBER),
 DLOCATION VARCHAR(50)
 );
+drop table DEPT_LOCATIONS;
+INSERT INTO DEPT_LOCATIONS
+(DNUMBER, DLOCATION)
+VALUES
+(1, 'Houston'),
+(4, 'Stafford'),
+(5, 'Bellaire'),
+(5, 'Sugarland'),
+(5, 'Houston');
 
 CREATE TABLE PROJECT (
 PNAME VARCHAR(50) NOT NULL,
@@ -36,13 +66,43 @@ DNUM INT,
 foreign key(DNUM) references DEPARTMENT(DNUMBER)
 );
 
+INSERT INTO PROJECT
+(PNAME, PNUMBER, PLOCTATION, DNUM)
+VALUES
+('ProductX', 1, 'Bellaire', 5),
+('ProductY', 2, 'Sugarland', 5),
+('ProductZ', 3, 'Houston', 5),
+('Computerization', 10, 'Stafford', 4),
+('Reorganization', 20, 'Houston', 1),
+('Newbenefits', 30, 'Stafford', 4);
+
 CREATE TABLE WORKS_ON (
-ESSN VARCHAR(50),
-foreign key(ESSN) references DEPARTMENT(DNUMBER),
+ESSN varchar(50),
+foreign key(ESSN) references EMPLOYEE(SSN),
 PNO INT,
 FOREIGN KEY(PNO) REFERENCES PROJECT(PNUMBER),
 HOURS INT
 );
+DROP TABLE WORKS_ON;
+INSERT INTO WORKS_ON
+(ESSN, PNO, HOURS)
+VALUES
+('123456789', 1, 32.5),
+('123456789', 2, 7.5),
+('666884444', 3, 40.0),
+('453453453', 1, 20.0),
+('453453453', 2, 20.0),
+('333445555', 2, 10.0),
+('333445555', 3, 10.0),
+('333445555', 10, 10.0),
+('333445555', 20, 10.0),
+('999887777', 30, 30.0),
+('999887777', 10, 10.0),
+('987987987', 10, 35.0),
+('987987987', 30, 5.0),
+('987654321', 30, 20.0),
+('987654321', 20, 15.0),
+('888665555', 20, NULL);
 
 CREATE TABLE DEPENDENT (
 ESSN VARCHAR(50),
@@ -52,6 +112,18 @@ SEX VARCHAR(50),
 BDATE DATE NOT NULL,
 RELATIONSHIP VARCHAR(50)
 );
+
+INSERT INTO DEPENDENT
+(ESSN, DEPENDENT_NAME, SEX, BDATE, RELATIONSHIP)
+VALUES
+('333445555', 'Alice', 'F', '1986-04-05', 'DAUGHTER'),
+('333445555', 'Theodore', 'M', '1983-10-25', 'SON'),
+('333445555', 'Joy', 'F', '1958-05-03', 'SPOUSE'),
+('987654321', 'Abner', 'M', '1942-02-28', 'SPOUSE'),
+('123456789', 'Michael', 'M', '1988-01-04', 'SON'),
+('123456789', 'Alice', 'F', '1988-12-30', 'DAUGHTER'),
+('123456789', 'Elizabeth', 'F', '1967-05-06', 'SPOUSE');
+
 ALTER TABLE DEPARTMENT
 ADD CONSTRAINT Employee_SSN
 FOREIGN KEY (MGRSSN)
