ecab7ab1697e23f1a635636aa5b6743aaca2f421
Kavyarai-11a <kavyarai812@gmail.com>
2026-09-21 21:45:48 +0530

    New Commit

--- DSA_Class_Tasks/Linked_header.c
+++ DSA_Class_Tasks/Linked_header.c
@@ -67,3 +67,81 @@ int InsertFront(List *l,int data)
     return 0;
 }
 
+int InsertEnd(List *l,int data)
+{
+    if(l == NULL)
+    {
+        return -3;
+    }
+
+    Node *newNode = createNode(data);
+    if(newNode == NULL)
+    {
+        return -1;
+    }
+
+    if(l->head == NULL)
+    {
+        l->head = newNode;
+        l->tail = newNode;
+        l->count++;
+        return 0;
+    }
+
+    Node *temp = l->tail;
+    temp->link = newNode;
+    l->tail = newNode;
+    l->count++;
+
+    return 0;
+}
+
+int InsertPos(List *l,int data,int Pos)
+{
+    if(Pos < 1)
+    {
+        return -2;
+    }
+
+    if(l == NULL)
+    {
+        return -3;
+    }
+
+    Node *newNode = createNode(data);
+    if(newNode == NULL)
+    {
+        return -1;
+    }
+
+    if(Pos == 1)
+    {
+        newNode->link = l->head;
+        l->head = newNode;
+        l->count++;
+        return 0;
+    }
+
+    Node *temp = l->head;
+    for(int i=1;i<Pos-1;i++)
+    {
+        if(temp == NULL)
+        {
+            return 2;
+        }
+        temp = temp->link;
+    }
+
+    if(temp == NULL)
+    {
+        return -2;
+    }
+
+    newNode->link = temp->link;
+    temp->link = newNode;
+    l->count++;
+
+    return 0;
+
+}
+
