428f5d893113d837565a16dd90ff02256e993fbe
Kavyarai-11a <kavyarai812@gmail.com>
2026-09-23 01:46:27 +0530

    New Commit

--- DSA_Class_Tasks/Link_gpt.c
+++ DSA_Class_Tasks/Link_gpt.c
@@ -752,6 +752,38 @@ int main()
         break;
 
         case 11:
+
+        int data,result;
+        printf("Enter data to insert : ");
+        scanf("%d",&data);
+        
+        result = SearchList(l,data);
+
+        if(result == 0)
+        {
+            printf("Inserted Succesfully\n");
+        }
+
+        else if(result == -1)
+        {
+            printf("Memory allocation for new node fail\n");
+        }
+
+        else if(result == -3)
+        {
+            printf("Null Error\n");
+        }
+
+        else if(result == -4)
+        {
+            printf("List is empty\n");
+        }
+
+        else if(result == -5)
+        {
+            printf("Data not found");
+        }
+
         break;
 
         case 12:
