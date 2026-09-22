825093db1a5b5c11a73279060fbf692ce69afbbc
Kavyarai-11a <kavyarai812@gmail.com>
2026-09-23 01:45:00 +0530

    New Commit

--- DSA_Class_Tasks/Link_gpt.c
+++ DSA_Class_Tasks/Link_gpt.c
@@ -723,7 +723,32 @@ int main()
 
         break;
 
-        case 10:
+        case 10: 
+
+        int result;
+
+        result = TransversList(l);
+
+        if(result == 0)
+        {
+            printf("Inserted Succesfully\n");
+        }
+
+        else if(result == -1)
+        {
+            printf("Memory allocation for new node fail\n");
+        }
+
+        else if(result == -3)
+        {
+            printf("Null Error\n");
+        }
+
+        else if(result == -4)
+        {
+            printf("List is empty\n");
+        }
+
         break;
 
         case 11:
