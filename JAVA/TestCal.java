67308ee421a223ea393d092876b27ffaf65bd938
Kavyarai-11a <kavyarai812@gmail.com>
2026-09-23 23:07:20 +0530

    New Commit

---
+++ JAVA/TestCal.java
@@ -0,0 +1,9 @@
+import mypack.Cal;
+
+public class TestCal {
+	public static void main(String[] args) {
+		Cal c = new Cal();
+		System.out.println("Addition = " + c.add(20,10));
+		System.out.println("Subtraction = " + c.sub(20,10));
+	}
+}
